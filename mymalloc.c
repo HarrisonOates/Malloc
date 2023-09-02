10707469423d9abc21a1da3fda5f31072ba382dc
Harrison Oates <48871176+HarrisonOates@users.noreply.github.com>
2023-09-02 21:32:48 +1000

    Most tests passing at the moment. TODO includes wiping memory, making sure block sizes contain space for the metadata, and coalescing works

--- mymalloc.c
+++ mymalloc.c
@@ -11,137 +11,137 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-/* Each block of memory */
-typedef struct Block {
+/* Each block of memory is sandwiched between a header and a footer */
+typedef struct Header {
   size_t size; /* if bit 0 is hot, the block is allocated */
+  struct Header* prev;
+  struct Header* next;
+} Header;
 
-  size_t prevSize; /* For constant time coalescing */
-  struct Block* prev;
-  struct Block* next;
-} Block;
-
-/* Fenceposts for a given chunk of memory */
-typedef struct Fencepost {
-  int flag;
-} Fencepost;
-
-/* Defining a linked list to get additional memory from the OS */
-typedef struct Chunk {
-  struct Chunk* prev;
-  struct Chunk* next;
-  Block* blockChunk;
-} Chunk;
+typedef struct Footer {
+  size_t size; /* if bit 0 is hot, the block is allocated */
+} Footer;
 
 
-const size_t kBlockMetadataSize = sizeof(Block);
+
+const size_t kBlockMetadataSize = sizeof(Header) + sizeof(Footer);
 const size_t kMaxAllocationSize = (32ull << 20) - kBlockMetadataSize; // 32MB - size metadata
 
 /* Function prototypes */
 inline static size_t round_up(size_t size, size_t alignment);
-static Block *getMemory();
-Block *split_block(Block *block, size_t size);
-void *getDataFromBlock(Block *block);
-void *getBlockFromData(void *ptr);
-void coalesce(Block* toCoalesce);
-bool isAllocated(Block* b);
-void toggleAllocated(Block* b);
+static Header *getMemory();
+Header *split_block(Header *block, size_t size);
+void coalesce(Header* toCoalesce);
+bool isAllocated(Header* b);
+void toggleAllocated(Header* b);
+Footer *getFooter(Header* h);
+void wipeBlock(Header* h, size_t size);
+void clearBit(Header* h);
 
-/* Our list start */
-static Chunk* chunkList = NULL;
 
+Header *root = NULL;
+
+// TODO - add the size of the block into the block & wipe the memory
 
 void *my_malloc(size_t size) {
   if (size == 0 || size > kMaxAllocationSize){
     return NULL;
   }
 
+  // Need to add in metadata into the equation
+
+  
   size = round_up(size, kAlignment);
   /* For a block size, if larger than this we should split */
-  size_t splitCon = 2*(size + sizeof(Block) - 2*sizeof(Block*)) + 1;
-
-  Block *b;
-
-  /* Set up the chunklist */
-  if (chunkList == NULL){
-    b = getMemory(); /* We set up the free list */
-    /* We can allocate immediately and save time */
-    if (b == NULL){
+  size_t splitCon = 2*(size + sizeof(Header) - 2*sizeof(Header*) + 1) + 1;
+  if (root == NULL){
+    Header *h = getMemory();
+    if (h == NULL){
+      errno = ENOMEM;
       return NULL;
     }
-    if (b->size > splitCon){
-      b = split_block(b, size); /* Returns a block of size `size + kMetadatasize - 2*sizeof(Block*)` */
+    if (size > splitCon){
+      h = split_block(h, size);
     }
-    toggleAllocated(b);
-    return (void *) (b + sizeof(b->size) + sizeof(b->prevSize));
+    wipeBlock(h, size);
+    toggleAllocated(h);
+    return ((void*) h) + sizeof(size_t);
+  }
+ 
+  
+  Header* traverse = root;
+  while (traverse->next != NULL && traverse->size < size){
+    traverse = traverse->next;
+  }
+  // We have one of two possibilities now 
+  if (traverse->size >= size){
+    if (size >= splitCon){
+      traverse = split_block(traverse, size);
+    }
+    wipeBlock(traverse, size);
+    toggleAllocated(traverse);
+    return ((void*) traverse) + sizeof(size_t);
   }
 
-  b = NULL;
-
-  /* Traverses the chunk list to find a block of the correct size */
-  Chunk *traverse = chunkList;
-  while (traverse->next != NULL){
-    /* Traverse the list of blocks looking for a free bit */
-    /* Issue here is setting the block pointer to a free element - this can be done by detecting if they share the same pointer */
-    Block *p = traverse->blockChunk;
-    while ((p-> next != NULL)  && p->size < size + sizeof(Block) - 2*sizeof(Block*)){ /* We save the memory from the */
-      p = p->next;
-    }
-
-    // We get to the point where either we have enough size or we get to move to the new chunk
-    if (p->size >= size + sizeof(Block) - 2*sizeof(Block*)){
-      // We have our value for b;
-      b = p;
-      break;
-    }
-    else {
-      traverse = traverse->next;
-    }
-  }
-
-  if (b == NULL){
-    /* Allocate more memory, in an identical fashion to the first setup */
-    b = getMemory(); /* We set up the free list */
-    /* We can allocate immediately and save time */
-    if (b == NULL){
-      // We throw the error we got from the previous value
+  Header *h = getMemory();
+    if (h == NULL){
+      errno = ENOMEM;
       return NULL;
     }
-    if (b->size > splitCon){
-      b = split_block(b, size); /* Returns a block of size `size + kMetadatasize - 2*sizeof(Block*)` */
+    if (size > splitCon){
+      h = split_block(h, size);
     }
-   
-  }
-  else {
-    /* Give b its value*/
-    /* Move the next and prev pointers around */
-    /* Previous pointer to hand stuff to each other */
-    ((b - (b->prevSize)) -> next) = (Block*) b + (b->size); /* previous next*/
-    (b + (b->size)) -> prev = b - (b->prevSize); /* next previous */
-  }
-
-  toggleAllocated(b);
-  return (void *) (b + sizeof(b->size) + sizeof(b->prevSize));
+  
+  wipeBlock(h, size);
+  toggleAllocated(h);
+  return ((void *) h) + sizeof(size_t);
 }
 
 void my_free(void *ptr) {
+  if (ptr == NULL){
+    return;
+  }
+  Header *h = (Header *) (((size_t) ptr) - sizeof(size_t)); /* The block is above the next and prev pointers that we set up */
+  toggleAllocated(h);
+  // coalesce(h);
+  
 
 }
 
 /* Coalesces contiguous free blocks */
-void coalesce(Block* toCoalesce){
+/* This is super buggy atm - need to fix */
+void coalesce(Header* toCoalesce){
+  /* Right block */
+  bool rightCoalesceFlag = false;
+  Header* rightBlock = toCoalesce + toCoalesce->size;
+  if (!isAllocated(rightBlock)){
+    toCoalesce->next = rightBlock->next;
+    toCoalesce->prev = rightBlock->prev;
+    toCoalesce->size += rightBlock->size;
+    getFooter(toCoalesce)->size = toCoalesce->size;
+    rightCoalesceFlag = true;
+  }
 
-  /* right block */
-  if (!isAllocated((Block*) (toCoalesce +(toCoalesce->size)))){
-    /* Transfer next pointers and absorb sizes */
-    toCoalesce->next = (toCoalesce +(toCoalesce->size))->next;
-    toCoalesce->size += (toCoalesce +(toCoalesce->size))->size;
-  }
   /* Left block */
-  if (!isAllocated((Block*) (toCoalesce - (toCoalesce->prevSize)))){
-    (toCoalesce - (toCoalesce->prevSize))->prev = toCoalesce->prev;
-    (toCoalesce - (toCoalesce->prevSize))->size += toCoalesce->size;
+  Header* leftBlock = (Header * )(toCoalesce - (((Header*) (toCoalesce - sizeof(Footer)))->size));
+  if (!isAllocated(leftBlock)){
+    /* We haven't set whether toCoalesce has a 'next' necessarily */
+    if (rightCoalesceFlag){
+      /* connect the left block's next and prev neighbours to each other, and then connect it to the so far contiguous block */
+      if (leftBlock->prev != NULL){
+        (leftBlock->prev)->next = leftBlock->next;
+      }
+      if (leftBlock->next != NULL){
+        (leftBlock->next)->prev = leftBlock->prev;
+      }
+    }
+    leftBlock->size += toCoalesce->size;
+    leftBlock->prev = toCoalesce->prev;
+    leftBlock->next = toCoalesce->next;
+    getFooter(leftBlock)->size = leftBlock->size;
   }
-  return;
+
+
 }
 
 /* Rounds a size up to the correct alignment value */
@@ -153,94 +153,108 @@ inline static size_t round_up(size_t size, size_t alignment) {
 /* Requests chunk of memory from the OS. Inserts into chunk list,
  * adds fenceposts, and returns a pointer to the block.
  */
-static Block *getMemory(){
+static Header *getMemory(){
   int prot = PROT_READ | PROT_WRITE;
   int flags = MAP_PRIVATE | MAP_ANONYMOUS;
   int fd = 0;
   off_t offset = 0;
 
   void *p = mmap(NULL, ARENA_SIZE, prot, flags, fd, offset);
-
   /* Can't allocate a new chunk from the OS :( */
   if (p == MAP_FAILED){
     errno = ENOMEM;
     return NULL;
   }
 
-  /* Create chunk metadata and add it to the chunklist*/
-  Chunk *c = p;
+  /* Set up the fenceposts of the block */
+  Footer *f1 = p;
+  f1->size = 1; /* This doesn't have to be proper; just enough to prevent coalesce from coalescing outside bounds*/
+  Footer *f2 = (Footer *) (((void *)f1) + ARENA_SIZE - sizeof(size_t));
+  f2->size = 1;
 
-  if (chunkList == NULL){
-    chunkList = c;
-    c->prev = NULL;
-    c->next = NULL;
+  /* Set up the header and footer on this block */
+  Header *h = p + sizeof(size_t);
+  h->size = ARENA_SIZE - 2*sizeof(size_t);
+  // Allocate the footer
+  Footer *f = getFooter(h);
+  f->size = h->size;
+  /* Append to front of the linked list */
+
+  if (root == NULL){
+    root = h;
   }
-  else { /* We traverse the list to place the new chunk at the end */
-    Chunk *traverse = chunkList;
-    while (traverse->next != NULL){
-      traverse = traverse->next;
-    }
-    traverse->next = c;
-    c->prev = chunkList;
-    c->next = NULL;
+  else {
+   // Come back to this - it's potentially buggy
+    Header* n = root->next;
+    root = h;
+    h->next = n;
   }
+  h->prev = NULL;
 
-  /* add fence posts either side of the new block */
-  Fencepost *f1 = (void *) (c + sizeof(Chunk));
-  f1->flag = 1;
-  Block* b = (void *) (f1 + sizeof(Fencepost));
-  b->size = ARENA_SIZE - 2 * (sizeof(Fencepost)) - sizeof(Chunk);
-  Fencepost *f2 = (void *) (b + b->size - sizeof(Fencepost));
-  f2->flag = 1;
-  b->prev = (void *) f1;
-  b->next = (void *) f2;
-
-  c->blockChunk = b;
-
-  return b;
+ // return c->blockChunk;
+ return h;
 }
 
-/* returns a block of the correct size, considering the saving of the next and prev pointers */
-Block *split_block(Block *block, size_t size){
+/* Returns a block of the correct size, considering the saving of the next and prev pointers.
+ * Assume that there is sufficient room in the block, checked by the calling function.
+ * Size is the amount of space for allocation requested.
+ */
+Header *split_block(Header *block, size_t size){
   size_t total_size = block->size;
-  Block *nextVal = block->next; /* Saving this as it will get overwritten shortly */
-  size_t blockPrevSize = block->prevSize;
-  Block *left = block;
-  left->size = total_size - size - kBlockMetadataSize + 2*sizeof(Block*);
-  left->prev = block->prev;
-  left->prevSize = blockPrevSize;
-  Block *right = (size_t) left + left->size;
+
+  /* Saving value of next block as it will get overwritten shortly */
+  Header* nextVal = block->next;
+
+  /* Left block */
+  Header *left = block;
+  left->size = total_size - size - kBlockMetadataSize + 2*sizeof(Header*);
+  Footer *leftFooter = getFooter(left);
+  leftFooter->size = left->size;
+
+  Header *right = left + left->size;
+  right->size = total_size - left->size - 2*sizeof(Header*);
+  Footer *rightFooter = getFooter(right);
+  rightFooter->size = right->size;
+
+  /* Setting up the linked list again */
   left->next = right;
-  right->prev = left;
-  right->prevSize = left->size;
   right->next = nextVal;
-  right->size = size + kBlockMetadataSize - 2*sizeof(Block*);
+  right->prev = left;
 
   return right;
 }
 
-/* Converts a block to a pointer to its data */
-void *getDataFromBlock(Block *block) {
-  return (void *)(((size_t)block) + kBlockMetadataSize);
-}
-
-/* Converts a pointer to a block's data to its block tag */
-void *getBlockFromData(void *ptr) {
-  return (Block *)(((size_t)ptr) - kBlockMetadataSize);
-}
-
 /* Returns if the block is allocated or not */
-bool isAllocated(Block* b){
-  return (0x1 & (b->size));
+bool isAllocated(Header* h){
+  return (0x1 & (h->size));
 }
 
+/* This was fixed by casting h to size_t */
+Footer *getFooter(Header* h){
+  return (Footer *) (((size_t) h) + (h->size) - sizeof(Footer));
+}
 /* Sets a block to allocated if it is free and vice versa */
-void toggleAllocated(Block* b){
-  if (isAllocated(b)){
-    b->size &= ~(1u);
+void toggleAllocated(Header* h){
+  Footer *f = getFooter(h);
+  if (isAllocated(h)){
+    h->size &= ~(1u);
+    f->size &= ~(1u);
   }
   else {
-    b->size |= 1u;
+    h->size |= 1u;
+    f->size |= 1u;
   }
   return;
+}
+
+void clearBit(Header* h){
+  Footer *f = getFooter(h);
+  h->size &= ~(1u);
+  f->size &= ~(1u);
+}
+
+/* Clears a block ready for the program to use it */
+// TODO - come back and work on it as it fails under 'exact'
+void wipeBlock(Header* h, size_t size){
+  // memset((void*) (h + sizeof(size_t)), 0, size);
 }
