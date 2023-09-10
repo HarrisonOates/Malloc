bf35818e5add6a715c6a24219815cb6c3bc91896
Harrison Oates <48871176+HarrisonOates@users.noreply.github.com>
2023-09-10 20:22:24 +1000

    Multiple free lists passing all tests, albeit with not all blocks

--- mymalloc.c
+++ mymalloc.c
@@ -39,8 +39,10 @@ Footer *getFooter(Header* h);
 void wipeBlock(Header* h, size_t size);
 void clearBit(Header* h);
 void *retrieveAndFormatNewBlock(size_t size);
+void setUpSizeClasses(size_t size);
 
-Header *root = NULL;
+Header* lists[N_LISTS]; /* Contains the root of each size class. We combine 8 and 16 bytes as they share the same block size */
+bool setUpList = false;
 
 void *my_malloc(size_t size) {
   if (size == 0 || size > kMaxAllocationSize){
@@ -51,17 +53,33 @@ void *my_malloc(size_t size) {
   /* For a block size, if larger than this we should split */
   size_t splitCon = 2*(kBlockMetadataSize - 2*sizeof(Header*)) + size + sizeof(size_t);
 
-  Header *h;
   /* Intialize the linked list */
-  if (root == NULL){
-    return retrieveAndFormatNewBlock(size);
-    if (h == NULL){
-      return NULL;
-    }
-    return (void*) (((size_t) h) + sizeof(size_t));
+  if (!setUpList){
+      setUpSizeClasses(size);
+      setUpList = true;
   }
   
   /* Otherwise we are going to have to traverse the list */
+  Header *root = NULL;
+  int sizeClass = 1;
+  if (size <= 16){
+    root = lists[1];
+  }
+  else {
+    for (int i = 1; i < N_LISTS; i++){
+      if ((i + 1) * 8 >= size && lists[i] != NULL){
+        root = lists[i];
+        sizeClass = i;
+        break;
+      }
+    }
+  }
+  
+
+  if (root == NULL){  /* We couldn't find an appropriate block */
+    return retrieveAndFormatNewBlock(size);
+  }
+
   Header *trav = root;
   while (trav->next != NULL){
     if (!isAllocated(trav) && trav->size >= kBlockMetadataSize + size - 2*sizeof(Header*)){
@@ -70,13 +88,10 @@ void *my_malloc(size_t size) {
     trav = trav->next;
   }
 
-  if (trav == NULL){
-    return retrieveAndFormatNewBlock(size);
-  }
 
   /* We found our element! */
   if (!isAllocated(trav) && trav->size >= kBlockMetadataSize + size - 2*sizeof(Header*)){
-    h = trav;
+    Header *h = trav;
     if (trav->size > splitCon){
       h = split_block(h, size);
     }
@@ -87,6 +102,8 @@ void *my_malloc(size_t size) {
     if (h->prev != NULL){
       h->prev->next = h->next;
     }
+    lists[sizeClass]=h->next;
+
 
     // wipeBlock(h);
     toggleAllocated(h);
@@ -151,6 +168,79 @@ void coalesce(Header* toCoalesce){
 inline static size_t round_up(size_t size, size_t alignment) {
   const size_t mask = alignment - 1;
   return (size + mask) & ~mask;
+}
+
+/*
+ * Initializes the size classes with a single large block for each class. 
+ * Input is a pointer to the block from getMemory(). Should be run once.
+ * 
+ */
+void setUpSizeClasses(size_t size){
+  int numElements = 10;
+  size_t runningSizeCount = 0;
+
+  Header *h;
+    if (size <= (8 << 20) - kBlockMetadataSize){
+      h = getMemory(0);
+    }
+    else {
+      h = getMemory(size / ( 1 << 20));
+    }
+    
+    if (h == NULL){
+      errno = ENOMEM;
+      abort();
+    }
+
+  /* Handle special case of the 8 and 16 byte blocks */
+  size_t ultimateSize = h->size;
+  h->size = 2 * (numElements * (16 + kBlockMetadataSize - 16)); /* 8 and 16 byte blocks */
+  Footer* f = getFooter(h);
+  f->size = h->size;
+  lists[1] = h;
+  runningSizeCount = h->size;
+  h = (h + h->size); 
+  h->prev = NULL;
+  h->next = NULL;
+  /* We whurr through 8mb, and anything beyond that we plug onto the end of the last list */
+  for (int i = 2; i < N_LISTS; i++){
+    /* For the algo to work we need to set up the first in each list before entering the loop */
+    Header *root = h;
+
+    size_t freeListBlockSize = 8*(i + 1) + kBlockMetadataSize - 16;
+    h->size = freeListBlockSize;
+    h->prev = NULL;
+    h->next = NULL;
+    f = getFooter(h);
+    f->size = h->size;
+    runningSizeCount += h->size;
+    h = h + h -> size;
+
+    Header *prev = root;
+    for (int j = 1; j < numElements; j++){
+      h->size = freeListBlockSize;
+      h->prev = prev;
+      h->next = NULL;
+      prev->next = h;
+      
+      f = getFooter(h);
+      f->size = h->size;
+      runningSizeCount += h->size;
+      prev = h;
+      h = h + h -> size;
+
+    }
+    lists[i] = root;
+  
+  }
+  /* We end up with a huge block unused, so we'll tag it on the end of the last list */
+  /*
+  h->size = ultimateSize - runningSizeCount;
+  f = getFooter(h);
+  f->size = h->size;
+  lists[58]->next = h;
+  h->prev = lists[58];
+  */
 }
 
 
@@ -228,6 +318,8 @@ static Header *getMemory(unsigned long long mb){
   f->size = h->size;
   /* Append to front of the linked list */
 
+  /* AFTER DINNER - COME BACK TO THIS */
+  /*
   if (root == NULL){
     root = h;
   }
@@ -238,6 +330,7 @@ static Header *getMemory(unsigned long long mb){
     h->next = n;
     n->prev = h;
   }
+  */
   h->prev = NULL;
   h->next = NULL;
 
